380a6e680ba57be3931f213ab816516f2d3a61e5
Patrick Amfo <patrick.amfo@icloud.com>
2025-03-15 16:32:04 +0000

    ammended seed file and fixed issue

--- db/data/challenges.rb
+++ db/data/challenges.rb
@@ -3,92 +3,106 @@ def challenges
     {
       name: "Replicate a famous artwork! ",
       description: "You are tasked with re-creating this famous artwork using the perfect prompt writing. Don't worry, we're going to share hint and instructions to help you writing a nuanced prompt. ",
-      length: 840,
-      category: "creative writing"
+      length: 14,
+      category: "Creative Writing"
     },
+
     {
       name: "Everyday art!",
       description: "Take a picture of a random object around you. Let's try to help you recreate this with the perfect prompt writing. Let's be as detailed and expressive as possible.",
-      length: 360,
-      category: "creative writing"
+      length: 6,
+      category: "Creative Writing"
     },
+
     {
       name: "Haiku is Art",
       description: "We will give you a food-inspired haiku so you recreate this as a visual mood board that matches its mood.",
-      length: 360,
-      category: "crafting"
+      length: 6,
+      category: "Crafting"
     },
+
     {
       name: "Art is Haiku",
       description: "We will give you a travel-inspired moodboard so you can recreate this as a haiku that matches its mood.",
-      length: 360,
-      category: "creative writing"
+      length: 6,
+      category: "Creative Writing"
     },
+
     {
       name: "Storytelling everything",
       description: "Check this video and its caption. Awkward right? Let's try to create a script to match this video.",
-      length: 840,
-      category: "creative writing"
+      length: 14,
+      category: "Creative Writing"
     },
+
     {
       name: "Emoji Storytelling",
       description: "Use five provided random emojis / animated icons to craft a short 50-word microstory. Bonus: Illustrate one scene from it.",
-      length: 360,
-      category: "creative writing"
+      length: 6,
+      category: "Creative Writing"
     },
+
     {
       name: "Sounds like Art",
       description: "Listen to this one-minute voice note describing a dream and start writing an interpretation of this dream. ",
-      length: 840,
-      category: "creative writing"
+      length: 14,
+      category: "Creative Writing"
     },
+
     {
       name: "A VOICE OF ART",
       description: "Listen to this one-minute voice note describing an abstract story and start writing the missing piece. ",
-      length: 360,
-      category: "creative writing"
+      length: 6,
+      category: "Creative Writing"
     },
+
     {
       name: "Reverse Storyboarding",
       description: "Check this  10-second silent clip and write an alternative script for it.",
-      length: 360,
-      category: "creative writing"
+      length: 6,
+      category: "Creative Writing"
     },
+
     {
       name: "Random Collage",
       description: "Create a collage with 3 random pictures from your iPhone",
-      length: 840,
-      category: "crafting"
+      length: 14,
+      category: "Crafting"
     },
+
     {
       name: "The Human portrait!",
       description: "Pick one portrait picture from your phone and start drawing alongside it to complement the story. You can only draw one element",
-      length: 840,
-      category: "crafting"
+      length: 14,
+      category: "Crafting"
     },
+
     {
       name: "Found Poetry!",
       description: "Here are 20 words that we carefully choose, arrange them to create a meaningful poem.",
-      length: 1920,
-      category: "crafting"
+      length: 32,
+      category: "Crafting"
     },
+
     {
       name: "Shadow stories",
       description: "Create hand shadows/animation photograph them, then digitally add elements to create a narrative scene.",
-      length: 1920,
-      category: "creative writing"
+      length: 32,
+      category: "Creative Writing"
     },
+
     {
       name: "Constraint Haiku",
       description: "Write a haiku on paper using only words that begin with the day's featured letter,",
-      length: 360,
-      category: "creative writing"
+      length: 6,
+      category: "Creative Writing"
     },
+    
     {
       name: "Pallete Challenge",
       description: "Find a colorful object in your home, extract its color palette digitally, then use those exact colors in a artwork.",
-      length: 840,
-      category: "crafting"
+      length: 14,
+      category: "Crafting"
     }
   ]
 end
