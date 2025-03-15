0a379d4d223bffe3beb3edc1b4b508e6003dedd2
Patrick Amfo <patrick.amfo@icloud.com>
2025-03-15 14:09:49 +0000

    Added the challenges

---
+++ db/data/challenges.rb
@@ -0,0 +1,94 @@
+def challenges
+  return [
+    {
+        name: "Tradition Artwork",
+        description: "",
+        length: 840,
+        category: "creative writing"
+    },
+    {
+        name: "Reframing Photo",
+        description: "",
+        length: 360,
+        category: "creative writing"
+    },
+    {
+      name: "Haiku to Art",
+      description: "",
+      length: 360,
+      category: "crafting"
+    },
+    {
+      name: "Audio to writing",
+      description: "",
+      length: 360,
+      category: "crafting"
+    },
+    {
+      name: "Audio to writing",
+      description: "",
+      length: 360,
+      category: "creative writing"
+    },
+    {
+      name: "Reverse storyboard",
+      description: "",
+      length: 840,
+      category: "creative writing"
+    },
+    {
+      name: "Create a collage",
+      description: "",
+      length: 360,
+      category: "crafting"
+    },
+    {
+      name: "Found potery",
+      description: "",
+      length: 360,
+      category: "crafting"
+    },
+    {
+      name: "Voice to visual",
+      description: "",
+      length: 840,
+      category: "crafting"
+    },
+    {
+      name: "Voice to visual",
+      description: "",
+      length: 840,
+      category: "creative writing"
+    },
+    {
+      name: "Handrawn to digital",
+      description: "",
+      length: 1920,
+      category: "crafting"
+    },
+    # {
+    #   name: "Shadow stories",
+    #   description: "",
+    #   length: 1920,
+    #   category: "creative writing"
+    # },
+    {
+      name: "Constraint Haiku",
+      description: "",
+      length: 360,
+      category: "creative writing"
+    },
+    {
+      name: "Pallete Challenge",
+      description: "",
+      length: 840,
+      category: "crafting"
+    },
+    {
+      name: "Emoji translator",
+      description: "",
+      length: 360,
+      category: "creative writing"
+    }
+  ]
+end
