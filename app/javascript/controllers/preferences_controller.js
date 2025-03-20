576ea0e2ac444c8b275c4b5cd1e8490fce182d63
Henry Thomas <henrykun95@gmail.com>
2025-03-20 22:47:51 +0000

    added some styling

--- app/javascript/controllers/preferences_controller.js
+++ app/javascript/controllers/preferences_controller.js
@@ -2,16 +2,25 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="preferences"
 export default class extends Controller {
-  static targets = [ "category", "update", "tagline" ];
+  static targets = [ "length", "category", "update", "tagline" ];
+
+  // connect() {
+  //   this.lengthTarget.classList.add('animation: fadeInOut 4s linear forwards;');
+  // }
 
   loadCategory() {
     this.categoryTarget.classList.remove("d-none");
+    this.categoryTarget.opacity = 0;
     this.categoryTarget.scrollIntoView();
-    this.taglineTarget.innerText = "What's your vibe? Pick your challenge type!"
+    this.taglineTarget.innerText = "What's your vibe? Pick your challenge type!";
   }
 
   categoryIntoView() {
     this.categoryTarget.scrollIntoView();
+  }
+
+  toggleLengthText() {
+    this.taglineTarget.innerText = "Fit this challenge into your routine and set the right time for you!"
   }
 
   loadUpdate() {
