3eb26d31e8083f33a9fb2ff1dcf91d99bec3a761
Alexander Tan <alexandertan527@gmail.com>
2025-03-22 16:15:35 +0000

    fixing model dependent destroys

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  has_many :user_challenges
+  has_many :user_challenges, dependent: :destroy
   has_many :challenges, through: :user_challenges
   validates :length_preference, inclusion: { in: [6, 14, 32], allow_blank: true }
   validates :category_preference, inclusion: { in: ["Crafting", "Creative Writing", "Creative Lesson"], allow_blank: true }
