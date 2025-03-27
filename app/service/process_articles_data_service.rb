e1ed69731d1fb25128ca740432259ed21fe5ad83
Henry Thomas <henrykun95@gmail.com>
2025-03-27 21:10:53 +0000

    changing heroku

--- app/service/process_articles_data_service.rb
+++ app/service/process_articles_data_service.rb
@@ -1,5 +1,5 @@
-# class ProcessArticleDataService
-#   def self.code
-#     data = ArticleApiService.fetch_data
-#   end
-# end
+class ProcessArticleDataService
+  def self.code
+    data = ArticleApiService.fetch_data
+  end
+end
