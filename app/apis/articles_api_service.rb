86f981b1e31e7077eb8dc191dfd4f2252e6abfb1
Henry Thomas <henrykun95@gmail.com>
2025-03-29 14:52:32 +0000

    deleted api

--- app/apis/articles_api_service.rb
+++
@@ -1,9 +0,0 @@
-require "json"
-require "rest-client"
-
-class ArticleApiService
-  def self.fetch_data
-    response = RestClient.get "https://newsapi.org/v2/top-headlines?sources=techcrunch&apiKey=239716d63e1e4a0d960b95e89cb342df"
-    return JSON.parse(response)
-  end
-end
