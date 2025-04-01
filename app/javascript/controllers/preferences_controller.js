ab35ec0b1fc9832b68f3cfb0dde04686905aa7b9
Henry Thomas <henrykun95@gmail.com>
2025-04-01 20:08:35 +0100

    worked on completed show page front end

--- app/javascript/controllers/preferences_controller.js
+++ app/javascript/controllers/preferences_controller.js
@@ -12,8 +12,13 @@ export default class extends Controller {
   }
 
   showLengthButton() {
+    this.lengthTickTarget.classList.remove("d-none");
     this.lengthTickTarget.classList.add("visible", "fa-fade");
     this.taglineTarget.innerText = "Fit this challenge into your routine and set the right time for you!"
+  }
+
+  scrollLength() {
+    this.lengthTarget.scrollIntoView();
   }
 
   loadCategory() {
@@ -25,14 +30,17 @@ export default class extends Controller {
 
   categoryIntoView() {
     this.categoryTarget.scrollIntoView();
+    this.categoryTickTarget.classList.remove("d-none");
     this.categoryTickTarget.classList.add("visible", "fa-fade");
   }
 
-  toggleLengthText() {
+  scrollCategory() {
+    this.categoryTarget.scrollIntoView();
   }
 
   loadUpdate() {
     let update = this.updateTarget;
+    update.classList.remove("d-none");
     update.classList.add("visible");
     update.scrollIntoView();
   }
