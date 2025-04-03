cf2c115a158b5eb22b76f87e8a7994df8f8d9f46
Henry Thomas <henrykun95@gmail.com>
2025-04-03 18:42:33 +0100

    dashboard styling

--- app/javascript/controllers/tab_controller.js
+++ app/javascript/controllers/tab_controller.js
@@ -3,15 +3,23 @@ import { Controller } from "@hotwired/stimulus"
 // Connects to data-controller="tab"
 export default class extends Controller {
 
-  static targets = [ "track", "unfinished" ]
+  static targets = [ "track", "unfinished", "trackText", "unfinishedText" ]
+
+  connect() {
+    this.unfinishedTextTarget.style.opacity = "0.5";
+  }
 
   loadLearningTrack() {
     this.trackTarget.classList.remove("d-none");
+    this.trackTextTarget.style.opacity = "1";
     this.unfinishedTarget.classList.add("d-none");
+    this.unfinishedTextTarget.style.opacity = "0.5";
   }
 
   loadUnfinished() {
     this.unfinishedTarget.classList.remove("d-none");
+    this.unfinishedTextTarget.style.opacity = "1";
     this.trackTarget.classList.add("d-none");
+    this.trackTextTarget.style.opacity = "0.5";
   }
 }
